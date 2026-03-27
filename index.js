c9708689a8942e698aec5f365922e9e1843d76ba
kimanzi <mwendwak439@gmail.com>
2026-03-27 16:45:38 +1400

    added commits

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const savebtn=document.getElementById ('btn-save')
 const input =document.getElementById ('input1')
 const tasksect=document.getElementById('taskContainer')
+const duedate=document.getElementById('tasktime')
 
-savebtn.addEventListener('click',(e)=>{
-    e.preventDefault();
+savebtn.addEventListener('click',()=>{
+    
 if(input.value ===''){
     alert("please input task")
 }
 else
 {  
     const taskdiv=document.createElement('div')
-    
-const save=document.createElement("p")
-save.innerHTML=input.value
+    taskdiv.setAttribute('class','subdiv')
+                    
+                const save=document.createElement("p")
+                save.setAttribute('class','peri')
+                save.innerHTML=input.value
+
+                        const date=document.createElement("p")
+                        date.setAttribute('class','peri')
+                        date.innerHTML=`📅due:${duedate.value}`
+
+
 
 const deletebtn=document.createElement("button")
 deletebtn.innerHTML="delete"
   
 taskdiv.appendChild(save);
-tasksect.appendChild(taskdiv)
+taskdiv.appendChild(date);
 taskdiv.appendChild(deletebtn);
+tasksect.appendChild(taskdiv)
 
 
 deletebtn.addEventListener('click',()=>{
